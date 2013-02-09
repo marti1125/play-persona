ff000b1982732f300cfbe0d12caafd969c939d17
Willy Aguirre <marti1125@gmail.com>
2013-02-09 16:51:38 -0500

    add securesocial plugin

---
+++ app/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class User extends Model{
+	
+	public String username;
+	public String password;
+	public String fullname;
+		
+}
