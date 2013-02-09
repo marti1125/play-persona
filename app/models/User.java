2579b8ce6bae9b6079c25592394d62e22e882a0f
Willy Aguirre <marti1125@gmail.com>
2013-02-09 17:42:17 -0500

    add crud for users

--- app/models/User.java
+++ app/models/User.java
@@ -5,10 +5,19 @@ import javax.persistence.Entity;
 import play.db.jpa.Model;
 
 @Entity
-public class User extends Model{
-	
+public class User extends Model {
+
+	public String email;
 	public String username;
 	public String password;
 	public String fullname;
-		
+
+	public User(String email, String username, String password, String fullname) {
+		super();
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+	}
+
 }
